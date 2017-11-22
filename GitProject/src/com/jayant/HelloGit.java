3a85145765976fe07be621f192afdc42cb125a02
suryajayants <suryajayant@gmail.com>
2017-11-22 10:34:12 +0530

    added new print out

--- GitProject/src/com/jayant/HelloGit.java
+++ GitProject/src/com/jayant/HelloGit.java
@@ -3,6 +3,7 @@ package com.jayant;
 public class HelloGit {
 	public static void main(String[] args) {
 		// Jayant Kumar
-		System.out.println("How are you Git");
+		System.out.println("How are you GitHub");
+		System.out.println("Wow");
 	}
 }
